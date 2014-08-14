269c6974d5b21be83a111e27ea82cfe4b93b7ef1
Jamie Kingston <jamie.kingston@gmail.com>
2014-08-14 13:24:17 -0400

    Bootstrapped the heck out of the views
    q
    :wq
    exit

--- app/controllers/tables_controller.rb
+++ app/controllers/tables_controller.rb
@@ -1,2 +1,77 @@
 class TablesController < ApplicationController
+before_action :find_table, only: [:show, :edit, :update, :destroy, :transition]
+
+  before_action :find_restaurant
+
+  def index
+    @tables = Table.all
+  end
+
+  def show
+    @appointment = Appointment.new
+  end
+
+  def new
+    @table = Table.new
+    @medications = Medication.all
+    @doctors = Doctor.all
+  end
+
+  def create
+    @table = @restaurant.tables.new(table_params)
+    if @table.save == true
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @medications = Medication.all
+    @doctors = Doctor.all
+  end
+
+  def update
+    if @table.update_attributes table_params
+      redirect_to restaurant_table_path(@restaurant, @table)
+    else
+      render :edit
+    end
+    # @prescription = Prescription.create prescription_params
+  end
+
+  def destroy
+    @table.destroy
+    redirect_to restaraunt_path(@restaurant)
+  end
+ 
+  def transition
+    event = params[:event]+'!'
+    @table.send(event.to_sym)
+    redirect_to restaurant_table_path(@restaurant, @table)
+  end
+
+private
+
+  def find_table
+    @table = Table.find params[:id]
+    # @prescriptions = Prescription.find params[:id]
+  end
+
+  def find_restaurant
+    @restaurant = Restaurant.find params[:restaurant_id]
+  end
+
+  # def find_prescriptions
+  #   @prescription = Prescription.find params[:]
+  # end
+
+  # def find_appointments
+
+  # end
+
+  def patient_params
+    params.require(:patient).permit(:lastname, :firstname, :description, :dob, :gender, :facility_id, {medication_ids: []}, {doctor_ids: []})
+
+  end
 end
--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,6 +1,7 @@
 class Menu < ActiveRecord::Base
 
   has_many :items, dependent: :destroy
+  
   has_many :restaurant_menus
   has_many :restaurants, through: :restaurant_menus
 
